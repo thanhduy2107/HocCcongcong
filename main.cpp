65672f7b08c7b2049cdc141be171d4e4c0521ff1
Phan Thanh Duy <thduy2107@gmail.com>
2026-08-21 09:01:59 +0700

    update project code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 
 int main()
 {
-    std::cout << "Hello, C++!";
+    //Header
+    std::cout << "====================\n";
+    std::cout << "  MY FIRST C++ APP\n";
+    std::cout << "====================\n";
+    
+    // Student Information
+    std::cout << "Name: Thanh Duy\n";
+    std::cout << "School: HCMUTE\n";
+    std::cout << "Major: Automotive Engineering\n";
+    std::cout << "Year: 2025-2029\n";
+
+    //Personal Information
+    std::cout <<"\n";
+    std::cout <<"My favourite language is C++\n";
+    std::cout <<"My favourite food is Pho\n";
+    std::cout <<"My favourite sport is Football\n";
+    std::cout <<"My target is to become a skilled C++ developer\n";
+
+    //Footer
+    std::cout <<"\n";
+    std::cout << "====================\n";
+    std::cout << "  KEEP CODING!\n";
+    std::cout << "====================\n";
+
     return 0;
 }
